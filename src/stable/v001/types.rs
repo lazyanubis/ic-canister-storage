93bf355d7284e14aa311f2f375a653a4be0248d1
anubis <anubis@users.noreply.github.com>
2024-03-27 22:27:19 +0800

    comments

--- src/stable/v001/types.rs
+++ src/stable/v001/types.rs
@@ -58,7 +58,7 @@ pub struct CanisterKit {
 // 能序列化的和不能序列化的放在一起
 // 其中不能序列化的采用如下注解
 // #[serde(skip)] 默认初始化方式
-// #[serde(skip, default="init_xxx")] 指定初始化方式
+// #[serde(skip, default="init_xxx_data")] 指定初始化方式
 // ! 如果使用 ic-stable-structures 提供的稳定内存，不能变更 memory_id 的使用类型，否则会出现各个版本不兼容，数据会被清空
 #[derive(Serialize, Deserialize)]
 pub struct InnerState {
