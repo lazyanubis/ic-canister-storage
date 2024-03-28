816970ad74c7c4ee99ffa2b63f00a39e75305114
anubis <anubis@users.noreply.github.com>
2024-03-28 12:06:30 +0800

    storage

--- src/stable/v001/types.rs
+++ src/stable/v001/types.rs
@@ -94,8 +94,17 @@ impl Default for InnerState {
 }
 
 use candid::CandidType;
+use ic_canister_kit::stable;
+
+const MEMORY_ID_ASSETS: MemoryId = MemoryId::new(0); // 存放实际文件，hash 为键
+
+fn init_assets_data() -> StableBTreeMap<SliceOfHashDigest, Vec<u8>> {
+    stable::init_map_data(MEMORY_ID_ASSETS)
+}
 
 // ============================== 文件数据 ==============================
+
+pub type SliceOfHashDigest = [u8; 4 + 32];
 
 #[derive(
     CandidType, Serialize, Deserialize, Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord,
@@ -112,21 +121,60 @@ mod assets {
     use candid::CandidType;
     use serde::{Deserialize, Serialize};
 
-    use super::HashDigest;
+    use crate::stable::v001::types::init_assets_data;
+
+    use super::{HashDigest, SliceOfHashDigest};
 
     // 单个文件数据
     #[derive(CandidType, Serialize, Deserialize, Debug, Clone)]
     pub struct AssetData {
-        data: Vec<u8>, // 实际数据
+        // 堆内存无数据，存放在稳定内存了
+    }
+
+    const MAX_BUCKET_SIZE: u64 = 1024 * 1024 * 2;
+
+    #[inline]
+    fn get_key(hash: &HashDigest, chunk: u32) -> SliceOfHashDigest {
+        let mut key = [0; 36];
+        key[..4].copy_from_slice(&chunk.to_be_bytes());
+        key[4..].copy_from_slice(&hash.0);
+        key
     }
 
     impl AssetData {
-        pub fn from(_hash: &HashDigest, data: Vec<u8>) -> Self {
-            Self { data }
+        pub fn from(hash: &HashDigest, data: Vec<u8>) -> Self {
+            // 切片
+            let size = data.len() as u64;
+            let chunks = size / MAX_BUCKET_SIZE;
+            let mut index = (0..chunks)
+                .map(|i| {
+                    let key = get_key(hash, i as u32);
+                    (key, MAX_BUCKET_SIZE * i, MAX_BUCKET_SIZE)
+                })
+                .collect::<Vec<_>>();
+            let remain = size - chunks * MAX_BUCKET_SIZE;
+            if 0 < remain {
+                let key = get_key(hash, chunks as u32);
+                index.push((key, MAX_BUCKET_SIZE * chunks, remain))
+            }
+
+            // 插入数据
+            for (key, offset, size) in index {
+                let offset = offset as usize;
+                let size = size as usize;
+                let data = data[offset..offset + size].to_vec();
+                ic_cdk::spawn(async move {
+                    let mut assets = init_assets_data();
+                    assets.insert(key, data);
+                });
+            }
+
+            // 返回空对象
+            AssetData {}
         }
         pub fn slice(
             &self,
-            _hash: &HashDigest,
+            hash: &HashDigest,
             data_size: u64,
             offset: usize,
             size: usize,
@@ -134,7 +182,33 @@ mod assets {
             assert!(offset < data_size as usize);
             let offset_end = offset + size;
             assert!(offset_end <= data_size as usize);
-            std::borrow::Cow::Borrowed(&self.data[offset..offset_end])
+
+            let mut result = vec![0; size];
+            let mut cursor = 0;
+
+            let assets = init_assets_data();
+
+            let mut last_chunk = offset as u64 / MAX_BUCKET_SIZE;
+            let mut offset = (offset as u64 - last_chunk * MAX_BUCKET_SIZE) as usize;
+            let mut size = size;
+            while 0 < size {
+                let remain = MAX_BUCKET_SIZE as usize - offset; // 本次最多可以取这么多
+                let fetch = std::cmp::min(size, remain); // 本次应该取的数据
+
+                let key = get_key(hash, last_chunk as u32);
+
+                #[allow(clippy::unwrap_used)] // ? SAFETY
+                let data = assets.get(&key).unwrap();
+
+                result[cursor..cursor + fetch].copy_from_slice(&data[offset..offset + fetch]);
+
+                cursor += fetch; // 修改结果写入位置
+                last_chunk += 1; // 修改为下一个块
+                offset = (offset + fetch) % MAX_BUCKET_SIZE as usize; // 修改并检查新的块偏移位置
+                size -= fetch; // 修改剩余的数据
+            }
+
+            std::borrow::Cow::Owned(result)
         }
     }
 }
