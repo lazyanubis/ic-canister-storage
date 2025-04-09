a14236a41c5d4c454631798a2fb5aebd1394706b
anubis <lazyanubis@users.noreply.github.com>
2025-04-09 19:49:23 +0800

    fix: ic_cdk::trap

--- src/stable/business.rs
+++ src/stable/business.rs
@@ -27,44 +27,44 @@ pub trait Business:
     }
 
     fn business_example_cell_query(&self) -> crate::stable::ExampleCell {
-        panic!("Not supported operation by this version.")
+        ic_cdk::trap("Not supported operation by this version.")
     }
     fn business_example_cell_update(&mut self, test: String) {
-        panic!("Not supported operation by this version.")
+        ic_cdk::trap("Not supported operation by this version.")
     }
 
     fn business_example_vec_query(&self) -> Vec<crate::stable::ExampleVec> {
-        panic!("Not supported operation by this version.")
+        ic_cdk::trap("Not supported operation by this version.")
     }
     fn business_example_vec_push(&mut self, test: u64) {
-        panic!("Not supported operation by this version.")
+        ic_cdk::trap("Not supported operation by this version.")
     }
 
     fn business_example_vec_pop(&mut self) -> Option<crate::stable::ExampleVec> {
-        panic!("Not supported operation by this version.")
+        ic_cdk::trap("Not supported operation by this version.")
     }
     fn business_example_map_query(&self) -> HashMap<u64, String> {
-        panic!("Not supported operation by this version.")
+        ic_cdk::trap("Not supported operation by this version.")
     }
     fn business_example_map_update(&mut self, key: u64, value: Option<String>) -> Option<String> {
-        panic!("Not supported operation by this version.")
+        ic_cdk::trap("Not supported operation by this version.")
     }
 
     fn business_example_log_query(&self) -> Vec<String> {
-        panic!("Not supported operation by this version.")
+        ic_cdk::trap("Not supported operation by this version.")
     }
     fn business_example_log_update(&mut self, item: String) -> u64 {
-        panic!("Not supported operation by this version.")
+        ic_cdk::trap("Not supported operation by this version.")
     }
 
     fn business_example_priority_queue_query(&self) -> Vec<crate::stable::ExampleVec> {
-        panic!("Not supported operation by this version.")
+        ic_cdk::trap("Not supported operation by this version.")
     }
     fn business_example_priority_queue_push(&mut self, item: u64) {
-        panic!("Not supported operation by this version.")
+        ic_cdk::trap("Not supported operation by this version.")
     }
     fn business_example_priority_queue_pop(&mut self) -> Option<crate::stable::ExampleVec> {
-        panic!("Not supported operation by this version.")
+        ic_cdk::trap("Not supported operation by this version.")
     }
 }
 
