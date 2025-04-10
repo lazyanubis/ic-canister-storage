35d0099d7cc7d6d6613266cc496d8da26deabf5b
anubis <lazyanubis@users.noreply.github.com>
2025-04-10 19:30:03 +0800

    chore: remove arg box

--- src/stable/mod.rs
+++ src/stable/mod.rs
@@ -20,7 +20,7 @@ pub use v001::types::*;
 pub enum State {
     V0(Box<v000::types::InnerState>),
     V1(Box<v001::types::InnerState>),
-    // * 👆👆 UPGRADE WARNING: 引入新版本
+    // *    👆👆 UPGRADE WARNING: 引入新版本
 }
 use State::*;
 
@@ -28,14 +28,14 @@ use State::*;
 pub enum InitArgs {
     V0(Box<v000::types::InitArg>),
     V1(Box<v001::types::InitArg>),
-    // * 👆👆 UPGRADE WARNING: 引入新版本
+    // *    👆👆 UPGRADE WARNING: 引入新版本
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, CandidType)]
 pub enum UpgradeArgs {
     V0(Box<v000::types::UpgradeArg>),
     V1(Box<v001::types::UpgradeArg>),
-    // * 👆👆 UPGRADE WARNING: 引入新版本
+    // *    👆👆 UPGRADE WARNING: 引入新版本
 }
 
 // 初始化
@@ -43,8 +43,8 @@ impl Initial<Option<InitArgs>> for State {
     fn init(&mut self, args: Option<InitArgs>) {
         match args {
             Some(args) => match (self, args) {
-                (V0(s), InitArgs::V0(arg)) => s.init(Some(arg)),
-                (V1(s), InitArgs::V1(arg)) => s.init(Some(arg)),
+                (V0(s), InitArgs::V0(arg)) => s.init(Some(*arg)),
+                (V1(s), InitArgs::V1(arg)) => s.init(Some(*arg)),
                 // ! 👆👆 新增版本需要添加默认的数据
                 _ => ic_cdk::trap("version mismatched"),
             },
@@ -71,8 +71,8 @@ impl Upgrade<Option<UpgradeArgs>> for State {
         match args {
             Some(args) => {
                 match (self, args) {
-                    (V0(s), UpgradeArgs::V0(arg)) => s.upgrade(Some(arg)),
-                    (V1(s), UpgradeArgs::V1(arg)) => s.upgrade(Some(arg)),
+                    (V0(s), UpgradeArgs::V0(arg)) => s.upgrade(Some(*arg)),
+                    (V1(s), UpgradeArgs::V1(arg)) => s.upgrade(Some(*arg)),
                     // ! 👆👆 新增版本需要添加默认的数据
                     _ => ic_cdk::trap("version mismatched"),
                 }
@@ -91,7 +91,7 @@ impl StateUpgrade<Option<UpgradeArgs>> for State {
         match self {
             V0(_) => 0,
             V1(_) => 1,
-            // * 👆👆! 升级需要在此添加版本号
+            // *   👆👆! 升级需要在此添加版本号
         }
     }
 
